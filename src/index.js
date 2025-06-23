557fff82fba7a91f2449058d0c5982f28bbb604c
Igor <80968651+webdevbiv@users.noreply.github.com>
2025-06-23 20:42:21 +0300

    ADD | fs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { writeFile, readFile } from 'fs/promises';
+
 const message = 'Hello, World!';
 
 function sum(a, b) {
@@ -5,3 +7,13 @@ function sum(a, b) {
 }
 console.log(message);
 console.log(`The sum of 5 and 3 is: ${sum(5, 3)}`);
+
+try {
+  await writeFile('output.txt', message);
+  console.log('Message written to output.txt');
+
+  const data = await readFile('output.txt', 'utf8');
+  console.log(`Read from file: ${data}`);
+} catch (error) {
+  console.error(error);
+}
